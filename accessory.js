dbec493a7e0a9db5b5f11da2b4b8bbc68a2bde6f
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2018-07-23 08:10:49 +0300

    Brought project into WebStorm

--- accessory.js
+++ accessory.js
@@ -1,43 +1,45 @@
 // Many thanks to Frédéric Barthelet for this blog post: http://blog.theodo.fr/2017/08/make-siri-perfect-home-companion-devices-not-supported-apple-homekit
 
-const Service, Characteristic;
- 
+const request = require('request');
+const url = require('url');
+let Service, Characteristic;
+
 module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("nexa-switch", "NexaSwitch", nexaSwitch);
+  homebridge.registerAccessory("nexa-switch", "NexaSwitch", NexaSwitch);
 };
 
-function nexaSwitch(log, config) {
+function NexaSwitch(log, config) {
   this.log = log;
   this.getUrl = url.parse(config['getUrl']);
   this.postUrl = url.parse(config['postUrl']);
 }
 
-nexaSwitch.prototype = {
+NexaSwitch.prototype = {
   getServices: function () {
     let informationService = new Service.AccessoryInformation();
     informationService
       .setCharacteristic(Characteristic.Manufacturer, "NEXA")
       .setCharacteristic(Characteristic.Model, "PER-1500")
       .setCharacteristic(Characteristic.SerialNumber, "745-295-196");
- 
+
     let switchService = new Service.Switch("NEXA Switch");
     switchService
       .getCharacteristic(Characteristic.On)
         .on('get', this.getSwitchOnCharacteristic.bind(this))
         .on('set', this.setSwitchOnCharacteristic.bind(this));
- 
+
     this.informationService = informationService;
     this.switchService = switchService;
     return [informationService, switchService];
-  }
+  },
   getSwitchOnCharacteristic: function (next) {
     const me = this;
     request({
         url: me.getUrl,
         method: 'GET',
-    }, 
+    },
     function (error, response, body) {
       if (error) {
         me.log('STATUS: ' + response.statusCode);
